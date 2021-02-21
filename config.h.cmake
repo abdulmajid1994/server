27566bbc22b4f1724c19fd89b4e95d820cc1bc6e
Daniel Black <daniel@mariadb.org>
2021-02-21 12:08:49 +1100

    missing configure HAVE_ALIGNED_MALLOC

--- config.h.cmake
+++ config.h.cmake
@@ -125,6 +125,7 @@
 #cmakedefine HAVE_ACCESS 1
 #cmakedefine HAVE_AIOWAIT 1
 #cmakedefine HAVE_ALARM 1
+#cmakedefine HAVE_ALIGNED_MALLOC 1
 #cmakedefine HAVE_ALLOCA 1
 #cmakedefine HAVE_BFILL 1
 #cmakedefine HAVE_INDEX 1
